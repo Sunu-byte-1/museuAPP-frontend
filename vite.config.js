1e6bdb433d10f7ae0f618da1602b48ce718dd9b5
Sunu-byte-1 <abdallahdiouf.dev@gmail.com>
2025-10-07 18:00:03 +0000

    fix: probleme de deploiement avec vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  base: '/', // ✅ essentiel pour Vercel
+  plugins: [tailwindcss()],
 })
